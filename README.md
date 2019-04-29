41ee7fb2f7521d2a6c93ee23d116f53924b9559a
chenyimuren <cq_pioneer@126.com>
2019-04-30 07:53:53 +0800

    3

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Day10
+# 配置文件
+https://developers.weixin.qq.com/miniprogram/dev/reference/configuration/app.html
+
+#tabBar配置
+
---
+++ app/pages/index/index.js
@@ -0,0 +1,76 @@
+// app/pages/index/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgUrls: ["/images/1.jpg", "/images/2.jpg", "/images/3.jpg"],
+    quickNav: [{ title: '发现课程', 'image': '/images/quickNav/kecheng.png', url: '#' }, { title: '名师', 'image': '/images/quickNav/mingshi.png', url: '#' }, { title: '发现课程', 'image': '/images/quickNav/kecheng.png', url: '#' }, { title: '名师', 'image': '/images/quickNav/mingshi.png', url: '#' }, { title: '发现课程', 'image': '/images/quickNav/kecheng.png', url: '#' }, { title: '名师', 'image': '/images/quickNav/mingshi.png', url: '#' }, { title: '发现课程', 'image': '/images/quickNav/kecheng.png', url: '#' }, { title: '名师', 'image': '/images/quickNav/mingshi.png', url: '#' }, { title: '发现课程', 'image': '/images/quickNav/kecheng.png', url: '#' }, { title: '名师', 'image': '/images/quickNav/mingshi.png', url: '#' }],
+   
+    courseContent: [{ title: '模具CAD', 'image': '/images/course/cad.jpg', url: '#',  }, 
+                    { title: 'Animate', 'image': '/images/course/animate.jpg', url: '#' }, 
+                    { title: 'illustrator高级', 'image': '/images/course/aig.jpg', url: '#' }, 
+                    { title: '商务应用', 'image': '/images/course/ppt.jpg', url: '#' }, 
+                    { title: 'illustrator初级', 'image': '/images/course/aic.jpg', url: '#' }, 
+                    { title: 'ug高级', 'image': '/images/course/ug.jpg', url: '#',  }, 
+                    { title: 'indesign基础', 'image': '/images/course/adobe.jpg', url: '#' },
+                    { title: '就业指导', 'image': '/images/course/work.jpg', url: '#' }]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
